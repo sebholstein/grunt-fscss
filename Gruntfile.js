97caea61ae16ffb15c0b805849cab1118663ade0
Sebastian Müller <info@sebastian-mueller.net>
2013-12-21 13:16:27 +0100

    chore(gruntfile): fix load position

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,12 @@ module.exports = function(grunt) {
         files: {
           'test/temp/style-conc-sep-fs.css': ['test/fixtures/style.css', 'test/fixtures/style2.css']
         }
-      }
+      },
+      testComment: {
+        files: {
+          'test/temp/style-comment.css': 'test/fixtures/style-comment.css',
+        }
+      },
     },
 
     clean: {
@@ -49,19 +54,23 @@ module.exports = function(grunt) {
       tests: ['test/*_test.js'],
     },
 
+    changelog: {
+      options: {
+        dest: 'README.md',
+        github: 'https://github.com/SebastianM/grunt-fscss',
+        editor: 'sublime -w'
+      }
+    }
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "test/temp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'fscss:test', 'fscss:testSeperator', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'fscss:test', 'fscss:testSeperator', 'fscss:testComment', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['test']);
