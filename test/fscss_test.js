704d79403577a8d110a3919e0c8736f139bf2934
Sebastian Müller <info@sebastian-mueller.net>
2014-04-10 19:27:05 +0200

    chore(test): fix line break errors

--- test/fscss_test.js
+++ test/fscss_test.js
@@ -11,21 +11,21 @@ exports.fscss = {
   basicTest: function(test) {
     test.expect(1);
     var generated = grunt.file.read('test/temp/style-fs.css');
-    var expected = grunt.file.read('test/expected/style.css');
+    var expected = grunt.file.read('test/expected/style.css').replace(/\r/g, '');
     test.equal(generated, expected, "should output a file as expected");
     test.done();
   },
   concTest: function(test) {
     test.expect(1);
     var generated = grunt.file.read('test/temp/style-conc-fs.css');
-    var expected = grunt.file.read('test/expected/style-conc.css');
+    var expected = grunt.file.read('test/expected/style-conc.css').replace(/\r/g, '');
     test.equal(generated, expected, "should concatinate multiple files to one file");
     test.done();
   },
   concSeperatorTest: function(test) {
     test.expect(1);
     var generated = grunt.file.read('test/temp/style-conc-sep-fs.css');
-    var expected = grunt.file.read('test/expected/style-conc-sep.css');
+    var expected = grunt.file.read('test/expected/style-conc-sep.css').replace(/\r/g, '');
     test.equal(generated, expected, "should concatinate multiple files to one file with a different seperator");
     test.done();
   }
